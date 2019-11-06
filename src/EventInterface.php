3429eed74f97098c7694682312ed018f146eaa2c
spontoreau <pontoreau.sylvain@gmail.com>
2019-11-06 16:15:41 +0100

    :recycle: Remove getStreamId

--- src/EventInterface.php
+++ src/EventInterface.php
@@ -6,7 +6,6 @@ namespace Traya;
 
 interface EventInterface
 {
-    function getStreamId(): string;
     function getType(): string;
     function getMetadata(): array;
     function getPayload(): array;
